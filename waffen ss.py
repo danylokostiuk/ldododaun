d3a7091db6382ed7b9a454ef5b5aadc9ce59d8ed
dodo <148619694+zxcldodo@users.noreply.github.com>
2023-11-10 15:24:07 +0200

    Update waffen ss.py

---
+++ waffen ss.py
@@ -0,0 +1,15 @@
+
+import random
+number = int(input(print("Napishi chislo:"))
+a = random.randint(1,100)
+atry = 5
+while arty => 0
+ if number != a
+    atry - 1
+    print("nepravilno")
+    number = int(input(print("Napishi chislo:"))
+elif number == a
+    print("win")
+    break
+else:
+    print("error")
