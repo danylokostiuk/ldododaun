3824b232f9d6a098ccf50311d9ca893b738dec93
danylokostiuk <104257322+danylokostiuk@users.noreply.github.com>
2023-11-10 15:37:09 +0200

    Update waffen ss.py

--- waffen ss.py
+++ waffen ss.py
@@ -1,15 +1,15 @@
-
 import random
-number = int(input(print("Napishi chislo:"))
+number = int(input())
 a = random.randint(1,100)
-atry = 5
-while arty => 0
- if number != a
-    atry - 1
-    print("nepravilno")
-    number = int(input(print("Napishi chislo:"))
-elif number == a
-    print("win")
-    break
-else:
-    print("error")
+print(a)
+atry = 4
+while 0 <= atry:
+    if number != a:
+        atry = atry - 1
+        print("nepravilno")
+        number = int(input())
+    elif number == a:
+        print("pravilno")
+        break
+    else:
+        print("error")
